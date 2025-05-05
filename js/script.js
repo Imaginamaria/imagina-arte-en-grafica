fa1b6f6c42fc8829065a76baccd626147e01f409
imaginamaria <emaria@imagina.com.uy>
2025-05-05 18:54:10 -0300

    subo archivos

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// animacion del icono bulb a pincel del index
+
+const icon = document.getElementById('icon');
+const icons = ['fa-lightbulb', 'fa-paintbrush'];
+let i = 0;
+
+setInterval(() => {
+  icon.classList.remove(icons[i % 2]);
+  i++;
+  icon.classList.add(icons[i % 2]);
+}, 2000);
+
+
+// animacion del texto tipeandose del index
+
+function runTypingEffect() {
+  // Definir el texto a escribir
+  const text = 'Imagina arte en gráfica.';
+  // Obtener el elemento donde se mostrará el texto
+  const typingElement = document.getElementById('typing-text');
+  // Definir la velocidad de escritura
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+// Función para escribir el texto
+// i es el contador de caracteres
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+// Ejecutar la animación al cargar la página
+document.addEventListener('DOMContentLoaded', runTypingEffect);
+
+
